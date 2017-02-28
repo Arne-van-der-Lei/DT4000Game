a4b481b2f56638ddbc2a1925dfe5fd6eae6c3ef5
houdhakker2 <arne@vanderlei.be>
2017-03-01 00:14:44 +0100

    update playercontroller

--- DT-Game/Assets/Scripts/PlayerController.cs
+++ DT-Game/Assets/Scripts/PlayerController.cs
@@ -10,22 +10,24 @@ public class PlayerController : MonoBehaviour {
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
-
     // Use this for initialization
     void Start () {
         _Controller = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        moveDirection.x = Input.GetAxis("Horizontal");
+        moveDirection.x *= speed;
+
         if (_Controller.isGrounded)
         {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
+            moveDirection.y = 0;
             if (Input.GetButton("Jump"))
                 moveDirection.y = jumpSpeed;
         }
+
         moveDirection.y -= gravity * Time.deltaTime;
         _Controller.Move(moveDirection * Time.deltaTime);
     }
