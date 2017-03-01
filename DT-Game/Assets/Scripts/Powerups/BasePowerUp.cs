e44a6fceeaf4f776255aebab88f623354f4143d5
houdhakker2 <arne@vanderlei.be>
2017-03-01 15:14:13 +0100

    little touch

--- DT-Game/Assets/Scripts/Powerups/BasePowerUp.cs
+++ DT-Game/Assets/Scripts/Powerups/BasePowerUp.cs
@@ -3,17 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class BasePowerUp : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
